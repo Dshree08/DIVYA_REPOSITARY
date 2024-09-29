53b686f21f9e6839041bc701eb7a0531e63847d8
Divya Shree M <Divya@gmail.com>
2024-09-30 02:40:06 +0530

    firstcommit

---
+++ project8/timeapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+import datetime
+def server_time(request):
+
+    server_time=datetime.datetime.now()
+    dict={"time":server_time}
+
+    return render(request=request,template_name="timeapp/time.html",context =dict)
